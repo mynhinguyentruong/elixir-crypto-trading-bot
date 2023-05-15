d94e71707297662a91045f30505a31e862771e1f
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-05-15 13:46:41 -0400

    Elixir Server basic

--- lib/app/kv_server.ex
+++ lib/app/kv_server.ex
@@ -1,14 +1,26 @@
-defmodule KVServer do
+# Do you know how to set up a server in Elixir
+
+defmodule Server do
   require Logger
 
-  def accept(port) do
-    # receive data as binary
-    # receive data line by line
-    # blocks on :gen_tcp.recv/2 until the data is available
-    #
+  # listen to connection
+  def listen(port) do
+    {:ok, socket} = :gen_tcp.listen(port, [:binary, packet: :line, active: false, reuseaddr: true])
+
+    accept_loop(socket)
   end
 
-  def loop_acceptor(socket) do
-    :gen_tcp.accept(socket)
+  # accept connection client
+  def accept_loop(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+
+    {:ok, data} = :gen_tcp.recv(client, 0)
+
+    :gen_tcp.send(client, data)
+
+    accept_loop(socket)
   end
+
+  # get data from client
+
 end
